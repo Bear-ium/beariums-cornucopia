dcc53f818f0c1b83f828fc02e4d9c3587d83d27a
Bear-ium <98127529+Bear-ium@users.noreply.github.com>
2026-08-19 12:23:58 +0200

    Added batch files to help with testing, Added Config file + config toggle for the planned advancement overhaul and updated ModID as it was still on the old project I took reference from at the start

--- src/main/java/com/beariumdev/beariumscornucopia/BeariumsCornucopia.java
+++ src/main/java/com/beariumdev/beariumscornucopia/BeariumsCornucopia.java
@@ -1,21 +1,26 @@
 package com.beariumdev.beariumscornucopia;
+import com.beariumdev.beariumscornucopia.Config;
+
 
 import org.slf4j.Logger;
 
+
 import com.mojang.logging.LogUtils;
+
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-
-
+import net.neoforged.fml.config.ModConfig;
 
 @Mod(BeariumsCornucopia.MODID)
 public class BeariumsCornucopia {
-    public static final String MODID = "beariumcraft";
+    public static final String MODID = "beariumscornucopia";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public BeariumsCornucopia(IEventBus modEventBus, ModContainer modContainer) {
         LOGGER.info("Bearium's Cornucopia loaded");
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.CONFIG_SPEC, MODID + ".toml");
     }
 }
---
+++ src/main/java/com/beariumdev/beariumscornucopia/Config.java
@@ -0,0 +1,36 @@
+package com.beariumdev.beariumscornucopia;
+
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.function.Supplier;
+
+public class Config {
+    public static final Config CONFIG;
+    public static final ModConfigSpec CONFIG_SPEC;
+
+    static {
+        Pair<Config, ModConfigSpec> pair = new ModConfigSpec.Builder().configure(Config::new);
+        CONFIG = pair.getLeft();
+        CONFIG_SPEC = pair.getRight();
+    }
+
+    private static <T> T category(ModConfigSpec.Builder builder, String name, String comment, Supplier<T> contents) {
+        builder.comment(comment).push(name);
+        T value = contents.get();
+        builder.pop();
+        return value;
+    }
+
+    public final ModConfigSpec.BooleanValue advancementOverhaulEnabled;
+
+    private Config(ModConfigSpec.Builder builder) {
+        advancementOverhaulEnabled = category(builder, "UI", "UI-related settings",
+                () -> category(builder, "AdvancementOverhaul", "AdvancementOverhaul Related Settings",
+                        () -> builder.comment("Enables the AdvancementOverhaul feature").define("enabled", true)));
+
+        // i dont like this indentation but visual studio "keeps on indenting" it like this so i have no choice
+        // well i do have a choice but thats a lot of work O_o
+    }
+}
