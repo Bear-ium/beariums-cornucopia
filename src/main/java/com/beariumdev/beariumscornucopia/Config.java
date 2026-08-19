117aba68fa2f6502468482429110ddb9716e4ba7
Bear-ium <98127529+Bear-ium@users.noreply.github.com>
2026-08-19 22:23:58 +0200

    updated config to be better and easier to use

--- src/main/java/com/beariumdev/beariumscornucopia/Config.java
+++ src/main/java/com/beariumdev/beariumscornucopia/Config.java
@@ -2,34 +2,26 @@ package com.beariumdev.beariumscornucopia;
 
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-import org.apache.commons.lang3.tuple.Pair;
-import java.util.function.Supplier;
-
 public class Config {
-    public static final Config CONFIG;
-    public static final ModConfigSpec CONFIG_SPEC;
+    public static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    private static void category(ModConfigSpec.Builder builder, String name, Runnable definitions) {
+        builder.push(name);
+        definitions.run();
+        builder.pop();
+    }
+
+    // #region Config Variables
+    public static ModConfigSpec.BooleanValue advancementOverhaulEnabled;
+    // #endregion
 
     static {
-        Pair<Config, ModConfigSpec> pair = new ModConfigSpec.Builder().configure(Config::new);
-        CONFIG = pair.getLeft();
-        CONFIG_SPEC = pair.getRight();
+        category(BUILDER, "Advancement Overhaul", () -> {
+            advancementOverhaulEnabled = BUILDER
+                    .comment("[Feature Toggle] Use the custom advancement screen")
+                    .define("advancementOverhaulEnabled", true);
+        });
     }
 
-    private static <T> T category(ModConfigSpec.Builder builder, String name, String comment, Supplier<T> contents) {
-        builder.comment(comment).push(name);
-        T value = contents.get();
-        builder.pop();
-        return value;
-    }
-
-    public final ModConfigSpec.BooleanValue advancementOverhaulEnabled;
-
-    private Config(ModConfigSpec.Builder builder) {
-        advancementOverhaulEnabled = category(builder, "UI", "UI-related settings",
-                () -> category(builder, "AdvancementOverhaul", "AdvancementOverhaul Related Settings",
-                        () -> builder.comment("Enables the AdvancementOverhaul feature").define("enabled", true)));
-
-        // i dont like this indentation but visual studio "keeps on indenting" it like this so i have no choice
-        // well i do have a choice but thats a lot of work O_o
-    }
+    public static final ModConfigSpec CONFIG_SPEC = BUILDER.build();
 }
